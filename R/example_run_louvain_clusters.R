a10658e5321a4a3380eea1eb70f74e0880f7d860
ttanya86 <44174022+ttanya86@users.noreply.github.com>
2021-12-09 16:15:28 -0500

    commented out

--- R/example_run_louvain_clusters.R
+++ R/example_run_louvain_clusters.R
@@ -2,20 +2,20 @@
 # Script to run louvain clustering
 #
 
-countsfp = ('./data/hu_colon_count_results/Export3_BiologicalProbeQC.xlsx')
-df = readxl::read_excel(countsfp, sheet=3)
-nfeatures = 2000
-npcs = 30
-roiid = 'SegmentDisplayName'
-x_col = 'ROICoordinateX'
-y_col = 'ROICoordinateY'
-df_spatial = readxl::read_excel(countsfp, sheet=1)
-df_spatial = df_spatial[, c(roiid, x_col, y_col)]
-col_pal = 'sunset'
-
-res = seurat_louvain(df, df_spatial, nfeatures, npcs)
-col_pal = color_parse(col_pal, n_cats=length(unique(res$cluster)))
-plot_clusters(res, col_pal)
+#countsfp = ('./data/hu_colon_count_results/Export3_BiologicalProbeQC.xlsx')
+#df = readxl::read_excel(countsfp, sheet=3)
+#nfeatures = 2000
+#npcs = 30
+#roiid = 'SegmentDisplayName'
+#x_col = 'ROICoordinateX'
+#y_col = 'ROICoordinateY'
+#df_spatial = readxl::read_excel(countsfp, sheet=1)
+# df_spatial = df_spatial[, c(roiid, x_col, y_col)]
+# col_pal = 'sunset'
+# 
+# res = seurat_louvain(df, df_spatial, nfeatures, npcs)
+# col_pal = color_parse(col_pal, n_cats=length(unique(res$cluster)))
+# plot_clusters(res, col_pal)
 
 
 
