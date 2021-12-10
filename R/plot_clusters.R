e3477976330cd05db5c6c4ff0313b1baa3bb5bf1
oospina <oscaredd_ol@yahoo.es>
2021-12-09 22:06:40 -0500

    coord_equal clusters

--- R/plot_clusters.R
+++ R/plot_clusters.R
@@ -6,7 +6,7 @@ plot_clusters = function(res, col_pal){
   ggplot2::ggplot(res) +
     ggplot2::geom_point(ggplot2::aes(x=x_pos, y=y_pos, color=cluster)) +
     ggplot2::scale_color_manual(values=col_pal) +
-    ggplot2::theme_classic() + 
-    ggplot2::scale_y_reverse()
+    ggplot2::theme_classic() +
+    coord_equal()
 }
 
